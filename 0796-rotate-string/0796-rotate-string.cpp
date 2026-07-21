60a28ec3e1e9fdd11f37ffc8002f2b01e1e8312e
Dev Kaushik <Devkaushik918@gmail.com>
2026-07-21 22:49:03 +0530

    Time: 2 ms (10.92%), Space: 8.2 MB (45.23%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -8,34 +8,10 @@ public:
             return false;
         }
         string concatenate = s +s;
-        int j=0;
-        int count=0;
-        for(int i=0;i<concatenate.length() ; i++)
-        {
-            if(concatenate[i] == goal[j])
-            {
-                count+=1;
-                j+=1;
-                if(j==m)
-                {
-                    break;
-                }
-            }
-            else 
-            {
-                count=0;
-                j=0;
-                if(concatenate[i] == goal[j])
-                {
-                    count+=1;
-                    j+=1;
-                }
-            }
-        }
-        if(count == m)
+        if (concatenate.find(goal) != string::npos)
         {
             return true;
-        }
+        }      
         else
         {
             return false;
